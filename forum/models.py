842dce0d0d60dfd7b7c98768b32c31b97e624e31
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-11-09 02:09:25 -0700

    Added forum model v1

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from course.models import Course
+
+# TODO
+# https://github.com/alex/django-taggit
+# https://github.com/shanbay/django-vote
+
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    parent = models.ForeignKey(Post, on_delete=models.CASCADE, empty=True)
+    anonymous = models.BooleanField(default=False)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
