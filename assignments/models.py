3bb8e893c070bd6723d7eba699e115593924567a
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-11-09 01:47:48 -0700

    Added assignment model v1

--- assignments/models.py
+++ assignments/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from course.models import Course
+
+class Assignment(models.Model):
+    name = models.CharField(max_length=64)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    published = models.BooleanField(default=False)
+    due = models.DateTimeField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class AssignmentAttachment(models.Model):
+    name = models.CharField(max_length=64)
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    file = models.FileField() # TODO
+
+    def __str__(self):
+        return self.name
+
+
+class AssignmentSubmission(models.Model):
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    file = models.FileField(blank=True) # TODO
+    content = models.TextField(blank=True)
+    comments = models.TextField(blank=True)
+    notes = models.TextField(blank=True)
+    grade = models.IntegerField(blank=True, null=True)
+    when = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+        if (self.content is None     and self.file is None) or \
+           (self.content is not None and self.file is not None):
+            raise ValidationError('Assignment submission must either be a text submission or a file.')
+
+    def __str__(self):
+        return assignment.name + ', ' + user.get_full_name()
+
+
--- course/models.py
+++ course/models.py
@@ -5,8 +5,8 @@ from alma.models import School
 
 class Course(models.Model):
     name = models.CharField(max_length=64)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
