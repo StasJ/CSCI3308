113f36521d0c17419acc952c48b98a93fd0489c7
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-11-13 01:37:47 -0500

    Added basic urls and base files for forum pages

--- course/urls.py
+++ course/urls.py
@@ -3,4 +3,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='course/index.html'), name='index'),
+    url(r'^(?P<course_id>[0-9]+)/$', TemplateView.as_view(template_name='course/index.html'), name='index'),
+    url(r'^(?P<course_id>[0-9]+)/', include([
+        url(r'^forum/', include('forum.urls')),
+    ])),
 ]
