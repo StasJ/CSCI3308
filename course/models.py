a375c402ea6e84b3d89ba40c3d7873ac51bc0a08
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-11-24 19:35:23 -0500

    Added course pin

--- course/models.py
+++ course/models.py
@@ -9,6 +9,7 @@ class Course(models.Model):
     title = models.CharField(max_length=64)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
+    pin = models.CharField(max_length=16, blank=True, null=True)
 
     def __str__(self):
         return self.department + ' ' + str(self.number) + ': ' + self.title;
