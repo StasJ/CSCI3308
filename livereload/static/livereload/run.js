ee52bb8164595782133ee46959f1c82e14a4f3af
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-12-05 13:30:31 -0700

    Added progressive timing to livereload

--- livereload/static/livereload/run.js
+++ livereload/static/livereload/run.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
+	function reload(fails) {
+		$.get('/livereload/', function(data) {
+			if (data == 1) {
+				location.reload();
+				fails = 0;
+			}
+			wait = fails <= 10 ? 500 : fails <= 40 ? 1000 : fails <= 100 ? 2000 : 5000;
+			setTimeout(function() {reload(fails+1);}, wait);
+		});
+	}
+	reload(0);
+	/*
 	setInterval(function() {
 		$.get('/livereload/', function(data) {
 			if (data == 1)
 				location.reload();
 		});
 	}, 1000);
+	*/
 });
