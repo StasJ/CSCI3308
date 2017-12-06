f08495a2f68cd7524b33d6776f35397ed8ef8539
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-12-05 19:56:03 -0700

    Added django.humanize and livereload to loaded modules

--- alma/settings.py
+++ alma/settings.py
@@ -37,14 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
     'taggit',
+    # 'material',
 
     'alma',
     'RTF',
     'course',
     'assignments',
     'forum',
+    'livereload',
 ]
 
 MIDDLEWARE = [
