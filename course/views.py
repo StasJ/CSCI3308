40f79bdd3f7ed93daa61f2574e6508c21b3f33a4
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-12-10 22:17:04 -0700

    Added basic course permission check

--- course/views.py
+++ course/views.py
@@ -2,10 +2,11 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from django.urls import reverse
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 
 from course.models import Course
 
-class CourseView(TemplateView):
+class CourseView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
     template_name = 'course/index.html'
 
     def get_context_data(self, course_id, **kwargs):
@@ -14,6 +15,9 @@ class CourseView(TemplateView):
         context['course'] = course
         context['course_list'] = Course.objects.all()
         return context
+
+    def test_func(self):
+        return self.request.user.course_set.filter(id=self.kwargs['course_id']).exists()
 
 def courseHomeRedirect(request):
     return HttpResponseRedirect(reverse('course_index', args=[1]))
