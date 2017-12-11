32a21cfd3f78fc974a5b6496fad232b008fd553a
Stanislaw Jaroszynski <stasj@ucar.edu>
2017-12-10 20:34:29 -0700

    Added course users model

--- course/models.py
+++ course/models.py
@@ -7,15 +7,31 @@ class Course(models.Model):
     department = models.CharField(max_length=4)
     number = models.IntegerField()
     title = models.CharField(max_length=64)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
     pin = models.CharField(max_length=16, blank=True, null=True)
+
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Membership')
 
     def __str__(self):
         return self.department + ' ' + str(self.number) + ': ' + self.title;
 
     def get_name(self):
         return self.department + ' ' + str(self.number)
+
+class Membership(models.Model):
+    STUDENT = 1
+    TA = 2
+    PROFESSOR = 3
+    TYPE_CHOICES = (
+            (STUDENT, 'Student'),
+            (TA, 'Teaching Assistant'),
+            (PROFESSOR, 'Professor')
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course,                 on_delete=models.CASCADE)
+    type = models.IntegerField(choices=TYPE_CHOICES, default=STUDENT)
+
 
 # class CourseResource(models.Model):
 
